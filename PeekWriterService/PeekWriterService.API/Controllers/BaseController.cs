1f69a09514555699387c08c6f1d7375e329818ea
Lucas Mendonca <ext.lucas.mendonca@farfetch.com>
2022-09-17 22:02:06 +0100

    initial commit

---
+++ PeekWriterService/PeekWriterService.API/Controllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Model.Responses.Common;
+using PeekWriterService.Models.Responses.Common;
+
+namespace PeekWriterService.API.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected ActionResult CustomResponse<T>(ResponseBase<T> response)
+        {
+            if (response.Errors != null && response.Errors.Any())
+            {
+                return BadRequest(new
+                {
+                    success = response.Success,
+                    errors = response.Errors.Select(n => n)
+                });
+            }
+
+            return Ok(new
+            {
+                success = response.Success,
+                data = response.Data
+            });
+        }
+    }
+}
---
+++ PeekWriterService/PeekWriterService.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PeekWriterService.Repository
+{
+    public class Class1
+    {
+    }
+}
